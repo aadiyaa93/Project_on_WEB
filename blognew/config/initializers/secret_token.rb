f35480250b9a8d8604e1694537418ef3da7b6ab1
Adiya Aitpayeva <aadiyaa93@gmail.com>
2012-11-22 02:29:57 -0500

    fourth commit

---
+++ blognew/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blognew::Application.config.secret_token = 'b6f623a8b9097b78e4393571e9e17c153ff003225edad3bef0e026d730f3462dcf3cb5a0b14ee0d1b5f5d8e8ea9bc6ecfb7a2c83a22f383fba75a9184bfe0b98'
